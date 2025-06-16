624bc01aa604a69cca9b700f191e939e319fc18e
biojimc <biojimc@gmail.com>
2025-06-16 22:40:22 +0800

    Update test_math.c
    use check functions

--- test/test_math.c
+++ test/test_math.c
@@ -1,22 +1,37 @@
-#include "unity.h"
+#include <check.h>
+#include "math_utils.h"
 
-// Function prototype
-int add(int a, int b);
-
-void setUp(void) {}
-void tearDown(void) {}
-
-void test_add_positive_numbers(void) {
-    TEST_ASSERT_EQUAL_INT(5, add(2, 3));
+START_TEST(test_add) {
+    ck_assert_int_eq(add(2, 3), 5);
+    ck_assert_int_eq(add(-1, -1), -2);
 }
+END_TEST
 
-void test_add_negative_numbers(void) {
-    TEST_ASSERT_EQUAL_INT(-5, add(-2, -3));
+START_TEST(test_subtract) {
+    ck_assert_int_eq(subtract(10, 4), 6);
+    ck_assert_int_eq(subtract(0, 0), 0);
+}
+END_TEST
+
+Suite *math_suite(void) {
+    Suite *s = suite_create("Math");
+    TCase *tc_core = tcase_create("Core");
+
+    tcase_add_test(tc_core, test_add);
+    tcase_add_test(tc_core, test_subtract);
+    suite_add_tcase(s, tc_core);
+
+    return s;
 }
 
 int main(void) {
-    UNITY_BEGIN();
-    RUN_TEST(test_add_positive_numbers);
-    RUN_TEST(test_add_negative_numbers);
-    return UNITY_END();
+    int number_failed;
+    Suite *s = math_suite();
+    SRunner *sr = srunner_create(s);
+
+    srunner_run_all(sr, CK_NORMAL);
+    number_failed = srunner_ntests_failed(sr);
+    srunner_free(sr);
+
+    return (number_failed == 0) ? 0 : 1;
 }
